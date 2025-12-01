a986ef820dd89e0930a0b993e6b76b346a9aacf7
Fabian <fabian.libert@eduhainaut.be>
2025-12-01 09:10:24 +0100

    test carte 3

--- GameScene.js
+++ GameScene.js
@@ -6,29 +6,28 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        // --- Configuration des Tuiles Animées (Plugin) ---
-        // Fichier AnimatedTiles.js à la racine
+        // Le plugin reste à la racine
         this.load.scenePlugin('AnimatedTiles', 'AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
         
-        // --- 1. Ressources du Joueur (à la racine) ---
-        this.load.atlas('ruby_walk', 'ruby_walk.png', 'ruby_walk_atlas.json'); 
-        this.load.json('ruby_walk_anim', 'ruby_walk_anim.json'); 
+        // --- 1. Ressources du Joueur (CORRIGÉ : dans tiny/) ---
+        this.load.atlas('ruby_walk', 'tiny/ruby_walk.png', 'tiny/ruby_walk_atlas.json'); 
+        this.load.json('ruby_walk_anim', 'tiny/ruby_walk_anim.json'); 
 
-        // --- 2. Ressources des Cartes Tiled (à la racine) ---
+        // --- 2. Ressources des Cartes Tiled (CORRIGÉ : dans tiny/) ---
         
-        // Charger TOUTES les images de tileset utilisées:
-        this.load.image('tileset_color1_key', 'Tilemap_color1.png'); 
-        this.load.image('tileset_color2_key', 'Tilemap_color2.png'); 
-        this.load.image('tileset_color3_key', 'Tilemap_color3.png'); 
-        this.load.image('tileset_color4_key', 'Tilemap_color4.png'); 
-        this.load.image('tileset_color5_key', 'Tilemap_color5.png'); 
-        this.load.image('tileset_water_foam_key', 'Water Foam.png'); 
-        this.load.image('tileset_water_bg_key', 'Water Background color.png'); 
-        this.load.image('tileset_tree1_key', 'Tree1.png');
-        this.load.image('tileset_tree2_key', 'Tree2.png');
-        this.load.image('tileset_tree3_key', 'Tree3.png');
+        // Charger TOUTES les images de tileset (PNG)
+        this.load.image('tileset_color1_key', 'tiny/Tilemap_color1.png'); 
+        this.load.image('tileset_color2_key', 'tiny/Tilemap_color2.png'); 
+        this.load.image('tileset_color3_key', 'tiny/Tilemap_color3.png'); 
+        this.load.image('tileset_color4_key', 'tiny/Tilemap_color4.png'); 
+        this.load.image('tileset_color5_key', 'tiny/Tilemap_color5.png'); 
+        this.load.image('tileset_water_foam_key', 'tiny/Water Foam.png'); 
+        this.load.image('tileset_water_bg_key', 'tiny/Water Background color.png'); 
+        this.load.image('tileset_tree1_key', 'tiny/Tree1.png');
+        this.load.image('tileset_tree2_key', 'tiny/Tree2.png');
+        this.load.image('tileset_tree3_key', 'tiny/Tree3.png');
 
-        // Charger les 5 fichiers JSON des cartes (à la racine)
+        // Charger les 5 fichiers JSON des cartes (Assumés à la racine)
         this.load.tilemapTiledJSON('map_up', 'test carte.json'); 
         this.load.tilemapTiledJSON('map_center', 'carte central.json'); 
         this.load.tilemapTiledJSON('map_right', 'carte droite.json'); 
